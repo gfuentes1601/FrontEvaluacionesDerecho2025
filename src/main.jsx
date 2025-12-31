a463cff652311c71ccc893568077cc16ba752bb4
Gerald <gerald.fuentes1601@alumnos.ubiobio.cl>
2025-12-31 01:17:26 -0300

    Avances en rediseño de Login

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
 
-import Root from '@pages/Root'
-import Login from '@pages/Login'
-import Home from '@pages/Home'
-import Simulacion from '@pages/Simulacion'
-import Resultado from '@pages/Resultado'
-import Error404 from '@pages/Error404'
+import Root from '@pages/Root';
+import Login from '@pages/Login';
+import Home from '@pages/Home';
+import Simulacion from '@pages/Simulacion';
+import Resultado from '@pages/Resultado';
+import Error404 from '@pages/Error404';
+import MantenerPreguntas from '@pages/MantenedorPreguntas';
 
-import '@styles/styles.css' //
-import MantenerPreguntas from './pages/MantenedorPreguntas'
+import '@styles/styles.css';
 
 const router = createBrowserRouter([
   {
@@ -17,30 +18,28 @@ const router = createBrowserRouter([
     element: <Root />,
     errorElement: <Error404 />,
     children: [
-      {
-        path: '/',
-        element: <Login />
-      },
-      {
-        path: '/home',
-        element: <Home />
-      },
-      {
-        path: '/preguntas',
-        element: <MantenerPreguntas />
-      },
-      {
-        path: '/simulacion',
-        element: <Simulacion />
-      },
-      {
-        path: '/resultado',
-        element: <Resultado />
-      }
+      { path: '/', element: <Login /> },
+      { path: '/home', element: <Home /> },
+      { path: '/preguntas', element: <MantenerPreguntas /> },
+      { path: '/simulacion', element: <Simulacion /> },
+      { path: '/resultado', element: <Resultado /> }
     ]
   }
-])
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
-)
+  <>
+    <Toaster
+      position="bottom-right"
+      reverseOrder={false}
+      limit={5}
+      toastOptions={{
+        duration: 3000,
+        style: {
+          fontSize: '14px',
+        },
+      }}
+    />
+    <RouterProvider router={router} />
+  </>
+);
